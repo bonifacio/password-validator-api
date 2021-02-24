a0f021ba8a97724bf472208940e4b5a3890ba16a
José Bonifácio Góes Júnior <boni.g.jr@gmail.com>
2021-02-24 18:57:19 -0300

    first commit

---
+++ src/main/java/com/boni/passwordvalidatorapi/PasswordValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.boni.passwordvalidatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordValidatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordValidatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boni/passwordvalidatorapi/PasswordValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boni.passwordvalidatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
